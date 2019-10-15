95ebd6b14c8ccbde8bb40d44b0a9c483d02b742b
gretard <gretard@users.noreply.github.com>
2019-10-16 01:31:16 +0300

    Fix handling when there are no values

--- src/sonar-sql-plugin/src/main/java/org/antlr/sql/sca/matchers/TextMatcher.java
+++ src/sonar-sql-plugin/src/main/java/org/antlr/sql/sca/matchers/TextMatcher.java
@@ -9,43 +9,45 @@ import org.sonar.plugins.sql.models.rules.RuleMatchType;
 
 public class TextMatcher implements IMatcher {
 
-	@Override
-	public boolean match(IParsedNode item, RuleImplementation ruleImplementation) {
+    @Override
+    public boolean match(IParsedNode item, RuleImplementation ruleImplementation) {
 
-		if (ruleImplementation.getRuleMatchType() == RuleMatchType.CLASS_ONLY) {
-			return true;
-		}
-		final org.sonar.plugins.sql.models.rules.TextCheckType type = ruleImplementation.getTextCheckType();
+        if (ruleImplementation.getRuleMatchType() == RuleMatchType.CLASS_ONLY) {
+            return true;
+        }
+        final org.sonar.plugins.sql.models.rules.TextCheckType type = ruleImplementation.getTextCheckType();
 
-		final String text = item.getText();
-		final List<String> names = ruleImplementation.getTextToFind().getTextItem();
+        final String text = item.getText();
+        final List<String> names = ruleImplementation.getTextToFind().getTextItem();
+        if (names.isEmpty()) {
+            return true;
+        }
 
-		
-		for (final String searchItem : names) {
-			switch (type) {
-			case DEFAULT:
-			case CONTAINS:
-				if (StringUtils.containsIgnoreCase(text, searchItem)) {
-					return true;
-				}
-				break;
+        for (final String searchItem : names) {
+            switch (type) {
+            case DEFAULT:
+            case CONTAINS:
+                if (StringUtils.containsIgnoreCase(text, searchItem)) {
+                    return true;
+                }
+                break;
 
-			case REGEXP:
-				if (text.matches(searchItem)) {
-					return true;
-				}
-				break;
-			case STRICT:
-				if (text.equalsIgnoreCase(searchItem)) {
-					return true;
-				}
-				break;
-			default:
-				break;
-			}
-		}
-		return false;
+            case REGEXP:
+                if (text.matches(searchItem)) {
+                    return true;
+                }
+                break;
+            case STRICT:
+                if (text.equalsIgnoreCase(searchItem)) {
+                    return true;
+                }
+                break;
+            default:
+                break;
+            }
+        }
+        return false;
 
-	}
+    }
 
 }
