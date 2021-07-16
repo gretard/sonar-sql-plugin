3ef66b7e9cebb4391548e6e6bf5bed1367026274
Eric Tummers <erictummers@me.com>
2021-07-16 15:05:02 +0200

    Register tag, severity, effort on internal rule

--- src/sonar-sql-plugin/src/main/java/org/sonar/plugins/sql/SQLRulesDefinition.java
+++ src/sonar-sql-plugin/src/main/java/org/sonar/plugins/sql/SQLRulesDefinition.java
@@ -1,8 +1,11 @@
 package org.sonar.plugins.sql;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 import org.antlr.sql.dialects.SQLDialectRules;
+import org.sonar.api.server.debt.DebtRemediationFunction;
+import org.sonar.api.server.debt.internal.DefaultDebtRemediationFunction;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.plugins.sql.models.rules.SqlRules;
 
@@ -19,8 +22,17 @@ public class SQLRulesDefinition implements RulesDefinition {
 
 			for (org.sonar.plugins.sql.models.rules.Rule rule : rulesDef.getRule()) {
 				NewRule x1Rule = repository.createRule(rule.getKey()).setName(rule.getName())
-						.setHtmlDescription(rule.getDescription());
-
+						.setHtmlDescription(rule.getDescription())
+						.addTags(rule.getTag())
+						.setSeverity(rule.getSeverity())
+						;
+				String gapMultiplier = rule.getDebtRemediationFunctionCoefficient();
+				String baseEffort = rule.getRemediationFunctionBaseEffort();
+				DebtRemediationFunction func = new DefaultDebtRemediationFunction(
+					DebtRemediationFunction.Type.valueOf(rule.getRemediationFunction()), 
+					(gapMultiplier == "") ? null : gapMultiplier, 
+					(baseEffort == "") ? null : baseEffort);		
+				x1Rule.setDebtRemediationFunction(func);
 				x1Rule.setActivatedByDefault(true);
 			}
 			repository.done();
