09ee525d147889b0da57aa79268959ce4703dfa1
gretard <gretard@users.noreply.github.com>
2024-01-29 22:57:29 +0200

    Update rule helper tool

--- src/sonar-sql-plugin/src/test/java/org/antlr/sql/tools/RulesHelperTool.java
+++ src/sonar-sql-plugin/src/test/java/org/antlr/sql/tools/RulesHelperTool.java
@@ -1,8 +1,10 @@
 package org.antlr.sql.tools;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Collection;
 import java.util.List;
 
@@ -18,74 +20,128 @@ import org.sonar.plugins.sql.models.rules.Rule;
 import org.sonar.plugins.sql.models.rules.SqlRules;
 
 public class RulesHelperTool {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        if (args.length != 4) {
-            System.out.println("Please pass the following: ");
-            System.out.println("\taction (print or  verify)");
-            System.out.println("\ttype (text or  file)");
-            System.out.println("\tvalue (sql string or path to folder) ");
-            System.out.println("\tdialect (tsql, pssql, mysql, pssql, pssqlv2) ");
+	public static void main(String[] args) throws FileNotFoundException, IOException {
 
-            System.out.println("Example:\r\nprint text \"SELECT * FROM dbo.test;\" tsql\r\n");
-            System.out.println("Example:\r\nverify file \"c:/tests/customRules.rules;\" mysql");
+		if (args.length < 4) {
+			System.out.println("Please pass the following: ");
+			System.out.println("\taction  (print, verify or analyze)");
+			System.out.println("\ttype    (text or file)");
+			System.out.println("\tvalue   (sql string or path to rules file/folder) ");
+			System.out.println("\tdialect (tsql, pssql, mysql, pssql, pssqlv2, snowflake)");
+			System.out.println("\tfolder  (folder to analyze, only applicable when using analyze action)");
 
-            return;
-        }
+			System.out.println("\r\nExample to print AST tree:\r\n\tprint text \"SELECT * FROM dbo.test;\" tsql");
 
-        String action = args[0];
-        String type = args[1];
-        String value = args[2];
-        String dialect = "tsql";
-        if (args.length > 3) {
-            dialect = args[3];
-        }
-        Dialects sqlDialect = Dialects.valueOf(dialect.toUpperCase());
+			System.out.println(
+					"\r\nExample to verify custom rules definitions:\r\n\tverify file \"c:/tests/customRules.rules;\" mysql");
+			System.out.println(
+					"\r\nExample to execute custom rules and plugin rules against specified folder:\r\n\tanalyze file \"c:/tests/customRules.rules;\" snowflake \"c:\\docs\\src\"");
+			System.out.println(
+					"\r\nExample to execute sql analysis againt specified folder:\r\n\tanalyze file \"NA\" snowflake \"c:\\docs\\src\"");
 
-        String text = value;
+			return;
+		}
 
-        if (!"text".equals(type)) {
-            text = IOUtils.toString(new FileInputStream(value), "UTF-8");
-        }
-        if ("print".equalsIgnoreCase(action)) {
-            System.out.println("Printing tree:\r\n");
-            AntlrContext ctx = sqlDialect.parse(text);
-            PrettyPrinter.print(ctx.root, 0, ctx.stream);
-            return;
+		String action = args[0];
+		String type = args[1];
+		String value = args[2];
+		String dialect = "tsql";
+		String folder = ".";
+		if (args.length > 3) {
+			dialect = args[3];
+		}
+		if (args.length > 4) {
+			folder = args[4];
+		}
+		Dialects sqlDialect = Dialects.valueOf(dialect.toUpperCase());
 
-        }
-        AdhocRulesProvider adhocRulesProvider = new AdhocRulesProvider();
-        List<SqlRules> rules = adhocRulesProvider.getAdhocRules(Constants.PLUGIN_SQL_EXTERNAL_RULES_SUFFIXES_DEFAULT,
-                ".", value);
-        System.out.println("text");
+		String text = value;
 
-        org.antlr.sql.sca.IssuesProvider issuesProvider = new IssuesProvider();
-        for (SqlRules rule : rules) {
-            System.out.println("Checking repository: " + rule.getRepoName());
-            for (Rule r : rule.getRule()) {
-                SqlRules temp = new SqlRules();
-                temp.getRule().add(r);
+		if (!"text".equals(type)) {
+			var file = new File(value);
+			if (file.exists()) {
+				text = IOUtils.toString(new FileInputStream(file), "UTF-8");
+			}
+		}
+		if ("print".equalsIgnoreCase(action)) {
+			System.out.println("Printing tree:\r\n");
+			AntlrContext ctx = sqlDialect.parse(text);
+			PrettyPrinter.print(ctx.root, 0, ctx.stream);
+			return;
 
-                System.out.println("Checking rule: " + r.getKey());
-                for (String s : r.getRuleImplementation().getCompliantRulesCodeExamples().getRuleCodeExample()) {
+		}
 
-                    AntlrContext ctx = sqlDialect.parse(s);
-                    Collection<SqlIssue> list = issuesProvider.check(RuleToCheck.createCodeList2(temp), ctx.root)
-                            .getaLLIssues();
+		AdhocRulesProvider adhocRulesProvider = new AdhocRulesProvider();
+		List<SqlRules> rules = adhocRulesProvider.getAdhocRules(Constants.PLUGIN_SQL_EXTERNAL_RULES_SUFFIXES_DEFAULT,
+				".", value);
+		org.antlr.sql.sca.IssuesProvider issuesProvider = new IssuesProvider();
 
-                    boolean res = list.size() == 0;
-                    System.out.println("\tc passed: " + res + " for " + s);
-                }
-                for (String s : r.getRuleImplementation().getViolatingRulesCodeExamples().getRuleCodeExample()) {
-                    AntlrContext ctx = sqlDialect.parse(s);
-                    Collection<SqlIssue> list = issuesProvider.check(RuleToCheck.createCodeList2(temp), ctx.root)
-                            .getaLLIssues();
-                    boolean res = list.size() > 0;
-                    System.out.println("\tv passed: " + res + " for " + s);
-                }
+		if ("verify".equalsIgnoreCase(action)) {
+			System.out.println("Verifying rules:\r\n");
+			for (SqlRules rule : rules) {
+				System.out.println("Checking repository: " + rule.getRepoName());
+				for (Rule r : rule.getRule()) {
+					SqlRules temp = new SqlRules();
+					temp.getRule().add(r);
 
-            }
-        }
+					System.out.println("Checking rule: " + r.getKey());
+					for (String s : r.getRuleImplementation().getCompliantRulesCodeExamples().getRuleCodeExample()) {
 
-    }
+						AntlrContext ctx = sqlDialect.parse(s);
+						Collection<SqlIssue> list = issuesProvider.check(RuleToCheck.createCodeList2(temp), ctx.root)
+								.getaLLIssues();
+
+						boolean res = list.size() == 0;
+						System.out.println("\tc passed: " + res + " for " + s);
+					}
+					for (String s : r.getRuleImplementation().getViolatingRulesCodeExamples().getRuleCodeExample()) {
+						AntlrContext ctx = sqlDialect.parse(s);
+						Collection<SqlIssue> list = issuesProvider.check(RuleToCheck.createCodeList2(temp), ctx.root)
+								.getaLLIssues();
+						boolean res = list.size() > 0;
+						System.out.println("\tv passed: " + res + " for " + s);
+					}
+
+				}
+			}
+			return;
+
+		}
+
+		if ("analyze".equalsIgnoreCase(action)) {
+			var fileDir = new File(folder);
+			System.out.printf("Analyzing folder: %s%n", fileDir);
+
+			var sqlDialectRulesWithCustomRules = sqlDialect.getDialectRules(rules.toArray(new SqlRules[0]));
+			var rulesToBeChecked = RuleToCheck.createCodeList2(sqlDialectRulesWithCustomRules);
+
+			var anyIssueFound = false;
+
+			var sourcePath = fileDir.toPath();
+			var filesToCheck = Files.walk(sourcePath).filter(f -> Files.isRegularFile(f)).toList();
+
+			for (var fileToCheck : filesToCheck) {
+				try {
+					var code = Files.readString(fileToCheck);
+					AntlrContext ctx = sqlDialect.parse(code);
+
+					var issues = issuesProvider.check(rulesToBeChecked, ctx.root);
+					for (var iss : issues.getaLLIssues()) {
+						System.out.printf("Issue found: \"%s(%s)\" at line %s in \"%s\" file%n", iss.getKey(),
+								iss.getName(), iss.getLine(), sourcePath.relativize(fileToCheck));
+						anyIssueFound = true;
+					}
+
+				} catch (Exception e) {
+					System.out.printf("Issue analyzing file (%s): %s%n", fileToCheck, e.getMessage());
+				}
+
+			}
+			if (anyIssueFound) {
+				System.exit(-1);
+			}
+		}
+
+	}
 
 }
